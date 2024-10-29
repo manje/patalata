29fe73f004060f699b530e9c8b2511cd24686276
manje <manje@manje.net>
2024-10-29 17:12:43 +0100

    documento de herramientas y portada

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -6,12 +6,37 @@ use App\Models\Tarea;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
+
+use League\CommonMark\CommonMarkConverter;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\Table\TableExtension;
+use League\CommonMark\Extension\Autolink\AutolinkExtension;
+use League\CommonMark\Extension\TaskList\TaskListExtension;
+use League\CommonMark\Extension\Strikethrough\StrikethroughExtension;
+
+
 class TareaController extends Controller
 {
     public function index()
     {
         $tareas = Tarea::with('dependencia')->withCount('usuariosQueVotaron')->orderByDesc('usuarios_que_votaron_count')->get();
-        return view('tareas.index', compact('tareas'));
+        // path del directorio de storage
+        $texto=file_get_contents(storage_path('app/private')."/herramientas.md");
+        $environment = new Environment([
+            'html_input' => 'allow', // Permite HTML embebido sin escape
+            'allow_unsafe_links' => false,
+        ]);
+        $environment->addExtension(new CommonMarkCoreExtension());  // Núcleo de CommonMark
+        $environment->addExtension(new TableExtension());           // Soporte para tablas
+        $environment->addExtension(new AutolinkExtension());        // Enlaces automáticos
+        $environment->addExtension(new TaskListExtension());        // Listas de tareas
+        $environment->addExtension(new StrikethroughExtension());   // Texto tachado
+        $converter = new CommonMarkConverter([], $environment);
+        $texto = $converter->convertToHtml($texto);
+        #return $texto;
+        return view('tareas.index', compact('tareas','texto'));
     }
 
     public function votar(Tarea $tarea)
--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -10,34 +10,6 @@
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
-    <div>
-        <div class="flex items-center">
-            <i class="fa-regular fa-calendar"></i>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                <a href="/agenda">Agenda</a>
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Un espacio donde reflejar las distintas actividades y convocatorias de los movimientos de cada localidad.
-        </p>
-
-    </div>
-
-    <div>
-        <div class="flex items-center">
-            <!-- solo icono free -->
-            <i class="fa-regular fa-comment"></i>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                <a href="/">Podcast</a>
-            </h2>
-        </div>
-
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Un espacio para la difusión de los podcast que distintos grupos de la ciudad realizan.
-        </p>
-
-    </div>
 
     <div>
         <div class="flex items-center">
@@ -46,7 +18,6 @@
                 <a href="https://tailwindcss.com/">Comunidad</a>
             </h2>
         </div>
-
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
             Un banco de talento y recursos de el que podrán hacer uso los distintos colectivos de la ciudad. Podrás inscribirte y ofrecer tus conocimientos, habitlidades y capacidades.
         </p>
@@ -64,4 +35,100 @@
             Todos los contenidos está vinculados a una ciudad, al registrarte eliges tu ciudad e intereses.
         </p>
     </div>
+
+    <div>
+        <div class="flex items-center">
+            <!-- solo icono free -->
+            <i class="fa-solid fa-people-group"></i>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                <a href="/">Equipos</a>
+            </h2>
+        </div>
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Las personas pertenecientes a colectivos, grupos de afinidad u otro tipo de proyectos pueden agruparse y publicar contenidos colectivamente.
+        </p>
+
+    </div>
+
+    <div>
+        <div class="flex items-center">
+            <!-- solo icono free -->
+            <i class="fa-solid fa-bullhorn"></i>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                <a href="/">Campañas</a>
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Los colectivos y movimientos podrán crear campañas dentro de la plataforma, y asociar contenidos (denuncias, eventos, podcasts, etc.) a la campaña
+        
+        </p>
+
+    </div>
+
+
+
+    <div>
+        <div class="flex items-center">
+            <i class="fa-regular fa-calendar"></i>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                <a href="/agenda">Agenda</a>
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Un espacio donde reflejar las distintas actividades y convocatorias de los movimientos de cada localidad.
+        </p>
+
+    </div>
+
+    <div>
+        <div class="flex items-center">
+            <!-- solo icono free -->
+            <i class="fa-solid fa-people-roof"></i>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                <a href="/">Denuncias</a>
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Herramienta para que al ciudadanía y movimientso puedan compartir con al comunidad denuncias públicas sobre cualquier asunto, adjuntando fotos y documentación.
+        </p>
+
+    </div>
+
+
+    <div>
+        <div class="flex items-center">
+            <!-- solo icono free -->
+            <i class="fa-solid fa-podcast"></i>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                <a href="/">Podcast</a>
+            </h2>
+        </div>
+
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Un espacio para la difusión de los podcast que distintos grupos de la ciudad realizan.
+        </p>
+    </div>
+
+    <div>
+        <div class="flex items-center">
+            <!-- solo icono free -->
+            <i class="fa-solid fa-comment-dots"></i>
+            <h2 class="ms-3 text-xl font-semibold text-gray-900">
+                <a href="/">Construcción colectiva</a>
+            </h2>
+        </div>
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            Iniciamos la construcción de esta herramienta en estrecha colaboración con personas que particpan de distintos movimientos sociales de la provincia de Cádiz.
+        </p>
+        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+            En nuestro pequeño <a href='/tareas'>gestor de tareas</a> puedes ver el borrador de la estructura de este proyecto y priorizar las areas a desarollar.
+            <br>
+            <!-- boton para ir a tareas -->
+            <a href="/tareas" class=" mt-4 btn btn-sx btn-primary ">Tareas</a>
+        </p>
+    </div>
+
 </div>
