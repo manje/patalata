db88eeb419b34a85503c40006a6f6fc4e62d0179
manje <manje@manje.net>
2024-11-20 13:32:19 +0100

    usabilidad

--- resources/views/livewire/teams/update-team-nameimage-form.blade.php
+++ resources/views/livewire/teams/update-team-nameimage-form.blade.php
@@ -8,19 +8,24 @@
     </x-slot>
 
     <x-slot name="form">
-        <!-- Team Owner Information -->
-        <div class="col-span-6">
-            <x-label value="{{ __('Team Owner') }}" />
 
-            <div class="flex items-center mt-2">
-                <img class="w-12 h-12 rounded-full object-cover" src="{{$team->owner->profile_photo_url }}" alt="{{ $team->owner->name}}">
 
-                <div class="ms-4 leading-tight">
-                    <div class="text-gray-900 dark:text-white">{{ $team->owner->name }}</div>
-                    <div class="text-gray-700 dark:text-gray-300 text-sm">{{ $team->owner->email }}</div>
-                </div>
-            </div>
+
+
+        <!-- Team Name -->
+        <div class="col-span-6 sm:col-span-4">
+            <x-label for="name" value="{{ __('Team Name') }}" />
+
+            <x-input id="name"
+                        type="text"
+                        class="mt-1 block w-full"
+                        wire:model="state.name"
+                        :disabled="! Gate::check('update', $team)" />
+
+            <x-input-error for="name" class="mt-2" />
         </div>
+
+
 
             <div x-data="{photoName: null, photoPreview: null}" class="col-span-6 sm:col-span-4">
                 <!-- Profile Photo File Input -->
@@ -66,18 +71,22 @@
 
 
 
-        <!-- Team Name -->
-        <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Team Name') }}" />
+        <!-- Team Owner Information -->
+        <div class="col-span-6">
+            <x-label value="{{ __('Team Owner') }}" />
 
-            <x-input id="name"
-                        type="text"
-                        class="mt-1 block w-full"
-                        wire:model="state.name"
-                        :disabled="! Gate::check('update', $team)" />
+            <div class="flex items-center mt-2">
+                <img class="w-12 h-12 rounded-full object-cover" src="{{$team->owner->profile_photo_url }}" alt="{{ $team->owner->name}}">
 
-            <x-input-error for="name" class="mt-2" />
+                <div class="ms-4 leading-tight">
+                    <div class="text-gray-900 dark:text-white">{{ $team->owner->name }}</div>
+                    <div class="text-gray-700 dark:text-gray-300 text-sm">{{ $team->owner->email }}</div>
+                </div>
+            </div>
         </div>
+
+
+
     </x-slot>
 
     @if (Gate::check('update', $team))
