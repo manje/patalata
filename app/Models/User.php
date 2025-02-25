90bacd5dc9c2cf21f239d3e2d29bac07aa3fb80b
manje <manje@manje.net>
2025-02-25 13:21:54 +0100

    feat: Obligatoriedad de confirmar email

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,7 +18,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use phpseclib3\Crypt\RSA;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens;
 
