0bca14a5375d0dd6915e3c87349e33d3e4e9668c
manje <manje@manje.net>
2025-08-24 16:43:33 +0200

    feat: documentacion

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -5,7 +5,11 @@
     </h1>
 
     <p class="mt-6 text-gray-500 leading-relaxed">
-        Esta es una maqueta en construcción de lo que será la nueva Patalata. Un espacio para el tejido social de la ciudad donde encontrarás una agenda, podcast, denuncias y que contará con herramientas para la movilización social.
+        Esta es una maqueta en construcción de lo que será la nueva Patalata. Un espacio para el tejido social de un territorio donde encontrarás una agenda, podcast, denuncias y que contará con herramientas para la movilización social.
+        <br>
+        Está construido sobre sofware libre, por lo que cualquiera puede crear su propio nodo de esta red, e implementa el protocolo ActivityPub de manera que los usuarios de distintas instancias pueden interactuar entre ellos e intercambiar contenido de manera que una herramientas como la agenda local incluirá eventos que se hayan creado e otros instancias o de cualquier usuario del fediverso.
+        <br>
+        Cada instacia debe ser moderada por cada colectivo que lo gestione, y gestionar la moderación junto con la sostenibilidad del nodo serán las principales retos una vez se ponga en marcha esta red. 
     </p>
 </div>
 
@@ -19,7 +23,13 @@
             </h2>
         </div>
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Un banco de talento y recursos de el que podrán hacer uso los distintos colectivos de la ciudad. Podrás inscribirte y ofrecer tus conocimientos, habitlidades y capacidades.
+            Un banco de talento y recursos de el que podrán hacer uso los distintos colectivos de la ciudad. Podrás inscribirte y ofrecer tus conocimientos, habilidades y capacidades.
+            <br>
+            Esta herramienta no estará federada, y los moderadores de la instancia deberán moderar las peticiones de ayuda para evitar abusos y saturación para lo que habrá que experimientar distintas técnicas.
+            <br>
+            Este módulo inicialmente no será federado por la complejidad de establecer mecanismos federados de moderación para evitar spam.
+            <br>
+            Se considera una herramienta importante y se comenzará a desarrolar una vez se termine el desarrollo de otros módulos como las campañas y las portadas territoriales.
         </p>
     </div>
 
@@ -32,7 +42,11 @@
         </div>
 
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Todos los contenidos está vinculados a una ciudad, al registrarte eliges tu ciudad e intereses.
+            Las instancias pueden crear portadas territoriales, que agrupan en un mismo espacio todos los contenidos geolocalizados en un territorio delimitado, distinguiendo distintos contenidos (artículos, eventos, denuncias) y constuyendo una agenda.
+            <br>
+            Al crear contenidos siempre se pueden geolocalizar, no solo eventos sino también otros contenidos como los artículos o las denuncias (que a nivel protocolo ActivityPub son también artículos). Esto permite que distintas instancias que se solapan en el territorio puedan incluir en las portadas territoriales contenidos de otras instancias.
+            <br>
+            La vocación es principalmente nodos centrados en una ciudad, pero se contempla que distintas instancias puedan solaparse territorialmente, por distintas razones. Por un lado un municipio puede tener un tamaño que permita instancias más pequeñas para barrios o pedanías, o existen espacios más amplios que una localidad que tienen dinámicas sociales que justifican un espacio común de intercambio.
         </p>
     </div>
 
@@ -46,6 +60,10 @@
         </div>
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
             Las personas pertenecientes a colectivos, grupos de afinidad u otro tipo de proyectos pueden agruparse y publicar contenidos colectivamente.
+            <br>
+            En principio no se plantea la creación de herramientas de trabajo en grupo, que tal complejidad sería demasiado ambicioso para este proyecto.
+            <br>
+            La principal funcionalidad de un equipo será el acceso al banco de talentos y recursos, la participación y creación de campañas y la gestión de una identidad colectiva en el fediverso.
         </p>
 
     </div>
@@ -60,8 +78,11 @@
         </div>
 
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Los colectivos y movimientos podrán crear campañas dentro de la plataforma, y asociar contenidos (denuncias, eventos, podcasts, etc.) a la campaña
-        
+            Los colectivos y movimientos podrán crear campañas dentro de la plataforma, y asociar contenidos (denuncias, eventos, podcasts, etc.) a la campaña.
+            <br>
+            Las campañas recopilan contenidos (eventos, artículos, etc.) de distintos movimientos de la ciudad. Las campañas tienes propietarios y miembros, los cuales pueden publicar contenido en la campaña, aunque cualquier otro colectivo puede publicar un contenido y asociarlo a una campaña, y serán los propietarios de la campaña los que moderen estos contenidos.
+            <br>
+            Las campañas se crean en la instancia del usuario o equipo que crea la campaña, pero usuarios y equipos de disintas instancias pueden añadirse como miembros o propietarios y en el caso de los propietarios gestionar la moderación de contenidos de no-miebmros enviados a la campaña así como añadir a nuevos miembros.
         </p>
 
     </div>
@@ -72,12 +93,12 @@
         <div class="flex items-center">
             <i class="fa-regular fa-calendar"></i>
             <h2 class="ms-3 text-xl font-semibold text-gray-900">
-                <a href="/agenda">Agenda</a>
+                <a href="/agenda">Agenda Local</a>
             </h2>
         </div>
 
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Un espacio donde reflejar las distintas actividades y convocatorias de los movimientos de cada localidad.
+            La agenda es un espacio común donde se agrupan todos los eventos asociados a una ciudad. La agenda trambién hará que los usuario registrados vean los eventos creados por cualquier perfil del fediverso a el que siga, y publique un evento.
         </p>
 
     </div>
@@ -92,7 +113,7 @@
         </div>
 
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-            Herramienta para que al ciudadanía y movimientso puedan compartir con al comunidad denuncias públicas sobre cualquier asunto, adjuntando fotos y documentación.
+            Herramienta para que al ciudadanía y movimientso puedan compartir con la comunidad denuncias públicas sobre cualquier asunto, adjuntando fotos y documentación.
         </p>
 
     </div>
@@ -109,6 +130,8 @@
 
         <p class="mt-4 text-gray-500 text-sm leading-relaxed">
             Un espacio para la difusión de los podcast que distintos grupos de la ciudad realizan.
+            <br>
+            Aunque agrupar podcast por territorio, o por campañas, puede ser una herramienta muy útil, es una de las herramientas menos prioritarias, comparadas con otras más imprescindibles, e incluso no necesaria en el primer lanzamiento.
         </p>
     </div>
 
---
+++ storage/app/private/herramientas.md
@@ -0,0 +1,73 @@
+* Registo de usuarios
+
+Los activistas que así lo deseen, además de registrarse con un perfil básico, al acceder podrán ver los contenidos de su localidad.
+
+Podrán darse de alta en una red de activistas, donde pueden elegir sus intereses, su disponibilidad para colaborar con otros movimientos y sus talentos para la construcción de un banco de talentos.
+
+Los intereses son una serie de categorías que hay que definir, y en las que se clasificarán todos los contenidos (feminismo, ecologismo, barrios, economía, cultura, pacifismo, etc. - categorias)
+
+En la disponibilidad el usuario puede elegir que tipo de solicitudes de colaboración está dispuesto a recibir (participar de crowfundings, realizar traslados en vehículos, colaborar en la organización de un evento, etc.)
+
+En el banco de talentos se podrán inscribir aquellos usuarios que sean expertos o tengan conocimientos en alguna disciplina que pueda ser demandada por los movimientos sociales (informáticos, abogados, técnicos medioambientales, educadores sociales, etc.)
+
+Los movimientos sociales podrán usar esta red para lanzar peticiones de ayuda, implementando las medidas necesarias para prevenir el abuso de esta herramienta como la saturación en el envío de mensajes, de manera que los usuarios puedan limitar tanto el número de mensajes que le lleguen, como la temática de estos.
+
+* Espacio colectivo
+
+Los usuarios pueden crear equipos, de estea manera podrán publicar contenidos de manera conjunta.
+
+Se creará un directorio de colectivos e inciativas en cada localidad.
+
+Los equipos podrán crear un perfil público, donde se pueda ver la información del colectivo, sus redes sociales, su web, etc. creando ente todos un directorio de los movimientos sociales en cada localidad.
+
+El sistema de roles inicial será Administrador y editor.
+
+¿Incluir el rol de miembro? 
+	Pros:
+		Las peticiones de ayuda a miembros del equipo/colectivo serían inmediatas.
+		Posibilitaría crear herramientas de colaboracíón interna como una agenda privada.
+	Contras:
+		En principio si no va a publicar contenido no tiene sentido que sea miembro del equipo.
+		Evitar la "colección" de miembros, como ocurre con los seguidores en redes sociales.
+		Antes de la creación de herramientas de trabajo interno hay mucho que construir.
+
+* Agenda
+
+En la agenda cualquier equipo o persona podrá publicar cualquier actividad, charlas, presentaciones de libros, manifestaciones, concentraciones, etc., cada evento pertenecerá a una ciudad y puede estar vinculado a una o más categorías.
+
+* Campañas
+
+Los equipos podrán crear campañas dentro de la plataforma, estas campañas pueden estar vinculadas a uno o más equipos. Los editores de los equipos podrán crear contenidos (eventos, denuncias, podcast, etc.) y vincularlo a una campaña.
+
+Los usuarios al crear cualquier contenido pueden vincularlo a una campaña, si el usuario no es editor o administrador de algun equipo vinculado a esta campaña, los editores recibirán una solicitud, que pueden aceptar o rechazar, para vincular ese contenido a la campaña.
+
+* Podcasts
+
+La plataforma contará con un espacio donde poder subir podcasts. Cualquier usuario o equipo puede crear un Podcast y añadir episodios. Los podcast estarán vinculados a una localidad y pueden estar vinculdos a varias categorias.
+
+Cada Episodios de un podcast pueden pueden una clasificación de categorias distanta, y puede estar vinculado a alguna de las campañas activas.
+
+Los podcasters deben asegurarse de que los episodios que suban no contengan materiales con derechos de autor que no tengan permiso para utilizar o distribuir.
+
+* Denuncia
+
+Un apartado permitirá a cualquier ciudadano realizar una denuncia púbica, ya sea solo con un texto, o adjuntando material multimedia.
+
+* Comentarios
+
+Todos los contenidos de la plataforma tendrá la opción de recoger comentarios de los usuarios de la plataforma.
+
+¿Se puede publicar un comentario a nombre de un equipo?
+	Pros:
+		Permite a un colectivo responder a un comentario para aclarar una información.
+	Contra:
+		Usuarios pueden usar la identidad de un equipo para publicar comentarios con opiniones personales.
+
+* Moderación
+
+Los contenidos publicados en patalata soportarán un sistema de moderación colectiva, que estará destinado a destacar los contenidos más importantes (cuando exista mucha cantidad de contenidos) y ocultar contenidos claramenta dañitos para la plataforma.
+
+Los usuarios tendrán un número de puntos limitados, todos los usuarios los mismos puntos, para durante un periodo de tiempo destacar los contenidos que consideren más importantes. El derecho a realizar funciones de moderación en la plataforma estará supeditado a ser usuarios activos de la plataforma y realizar un buen uso de la platforma en general y de las herramientas de moderación en particular.
+
+Los distintos grupos en cada localidad observarán que se está realizando un buen uso del sistema de moderación, y se intervendrá en el caso que el sistema de moderación sea usado de forma no legítima de manera que se estén tratando de ocultar contenidos legítimos por rivalidades varias.
+
